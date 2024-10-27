4b3b1b1d630c9519681a63db9d078cc755b0b55e
ShakirAnter <shakiranter@gmail.com>
2024-10-27 13:05:56 +0300

    Changed MongoDB URI to the live link

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -4,12 +4,13 @@ const User = require("../models/userModel");
 
 const addToWishList = async (req, res) => {
   try {
-    
     const user = await User.findById(req.user.id);
     const product = await Product.findById(req.body.productId);
 
-
-
+    // Check if the user and product exist
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
   } catch (error) {
     console.error(error);
   }
